cbcc1b982f90996d8ee6e683d30a80734b95a033
Jessie <jessie.wang@hotmail.co.uk>
2018-10-01 11:22:18 +1000

    bugs fixed

--- Jessie/wk02/4-thu/bankapp/app.js
+++ Jessie/wk02/4-thu/bankapp/app.js
@@ -37,12 +37,11 @@ function deposit(account, amount) {
 }
 
 // Add functionality so that a user can withdraw money from one of the bank accounts.
+// Make sure the balance in an account can't go negative. If a user tries to withdraw more money than exists in the account, ignore the transaction.
 
 function withdrawFromChecking() {
-	// balances[account] -= parseFloat(amount);
 	const withdrawalAmount = parseFloat(checkingElements.input.value);
 	const amountLeft = balances[BALANCE_CHECKING] - withdrawalAmount;
-
 	// Nothing left in checking
 	if (amountLeft <= 0) {
 		// Savings has enough funds
@@ -55,11 +54,10 @@ function withdrawFromChecking() {
 			message.innerHTML = 'NO moneyz';
 		}
 	// Still money left in checking
-	} 
-	else {
+	} else {
 		balances[BALANCE_CHECKING] = amountLeft;
 	}
-	updateDisplay()
+	updateDisplay();
 }
 
 // Make sure you are updating the display and manipulating the HTML of the page so a user can see the change.
@@ -68,51 +66,19 @@ function withdrawFromChecking() {
 function updateDisplay() {
 	savingsElements.balance.innerHTML = '$' + balances[BALANCE_SAVINGS].toFixed(2);
 	checkingElements.balance.innerHTML = '$' + balances[BALANCE_CHECKING].toFixed(2);
-	// how to set input value back to nothing after event is ran
-	// savings.input.value = '';
-	// savings.input.value = '';
-
-	balances[BALANCE_CHECKING] <= 0 ? checkingElements.background.style.backgroundColor 
-	= '#d95555' : checkingElements.background.style.backgroundColor = '#C0C0C0';
+	
+	balances[BALANCE_CHECKING] <= 0 ? checkingElements.background.style.backgroundColor = '#d95555' 
+	: checkingElements.background.style.backgroundColor = '#C0C0C0';
 }
-
-// Make sure the balance in an account can't go negative. If a user tries to withdraw more money than exists in the account, ignore the transaction.
-// Make sure there is overdraft protection going both ways.
-
-/*
-function withdrawProtection(account, amount) {
-	const withdrawalAmount = parseFloat(amount);
-	const amountLeft = balances[account] - withdrawalAmount;
-	// const withdrawalAmount = parseFloat(checkingElements.input.value);
-	// const amountLeft = balances[BALANCE_CHECKING] - withdrawalAmount;
-
-	// Nothing left in checking
-	if (amountLeft <= 0) {
-		// Savings has enough funds
-		if (balances[BALANCE_SAVINGS] > amountLeft) {
-			balances[BALANCE_SAVINGS] += amountLeft;
-			balances[BALANCE_CHECKING] = 0;
-			console.log('New checking', balances[BALANCE_CHECKING] + amountLeft);
-		} else {
-			console.log('NO moneyz');
-		}
-	// Still money left in checking
-	} 
-	else {
-		balances[BALANCE_CHECKING] = amountLeft;
-	}
-	updateDisplay()
-}
-*/
 
 function transferFunds(accountFrom, accountTo, accountFromElem, accountToElem, amount) {
 	if (balances[accountFrom] >= parseFloat(amount)) {
 		balances[accountFrom] -= parseFloat(amount);
 		accountFromElem.balance.innerHTML = '$' + balances[accountFrom].toFixed(2);
-		// accountToElem not updating right
-		accountToElem.balance.innerHTML = '$' + parseFloat(amount) + balances[accountTo].toFixed(2);
-		message.innerHTML = 'You are transferring $' + amount + ' from ' 
-		+ accountFrom + ' to ' + accountTo;
+		balances[accountTo] += parseFloat(amount);
+		accountToElem.balance.innerHTML = '$' + (balances[accountTo].toFixed(2));
+
+		message.innerHTML = 'You are transferring $' + amount + ' from ' + accountFrom + ' to ' + accountTo;
 	} else {
 		message.innerHTML = 'You have insufficient funds';
 	}
@@ -126,11 +92,19 @@ function savingsAccInterest() {
 	if (balances[BALANCE_SAVINGS] > 0) {
 		setInterval(() => {
 			balances[BALANCE_SAVINGS] = balances[BALANCE_SAVINGS].toFixed(2) * 1.1;
+			savingsElements.balance.innerHTML = '$' + balances[BALANCE_SAVINGS].toFixed(2);
+			// console.log(balances[BALANCE_SAVINGS])
 			if (balances[BALANCE_SAVINGS] > interestIncrement) {
 				savingsElements.background.style.backgroundColor = 'gold'
 			}
 		}, 5000);
 	}
+}
+
+function resetElems() {
+	// how to set input value back to nothing after event is ran
+	// savings.input.value = '';
+	// savings.input.value = '';
 }
 
 /*
@@ -156,19 +130,17 @@ checkingElements.btnDeposit.addEventListener('click', () => {
 
 checkingElements.btnWithdraw.addEventListener('click', () => { 
 	withdrawFromChecking();
-	// withdrawProtection(BALANCE_CHECKING, checkingElements.input.value);
 });
 
 checkingElements.btnTransfer.addEventListener('click', () => {
-	transferFunds(BALANCE_CHECKING, BALANCE_SAVINGS, checkingElements, savingsElements, checkingElements.input.value)
+	transferFunds(BALANCE_CHECKING, BALANCE_SAVINGS, checkingElements, savingsElements, checkingElements.input.value);
 });
 
 savingsElements.btnDeposit.addEventListener('click', () => { 
 	deposit(BALANCE_SAVINGS, savingsElements.input.value);
-	savingsAccInterest();
+	// savingsAccInterest();
 });
 
 savingsElements.btnTransfer.addEventListener('click', () => {
 	transferFunds(BALANCE_SAVINGS, BALANCE_CHECKING, savingsElements, checkingElements, savingsElements.input.value);
-	// withdrawProtection(BALANCE_SAVINGS, savingsElements.input.value);
 });
