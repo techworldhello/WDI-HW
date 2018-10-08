c248baf2807ff556e37e2de038973cd818c7ffff
Jessie <jessie.wang@hotmail.co.uk>
2018-10-08 22:07:41 +1100

    updated bonus section

--- Jessie/wk04/1-mon/first_ruby_hw/temperature_converter.rb
+++ Jessie/wk04/1-mon/first_ruby_hw/temperature_converter.rb
@@ -49,24 +49,24 @@ p temp_features[:unit]
 # When the program starts, the user should be prompted to enter a temperature OR quit the program.
 # After the program displays the starting/converted temperatures, it should return to the initial user prompt.
 
-# not_quitting_program = true
+not_quitting_program = true
 
-# while not_quitting_program
-# 	puts "what's the temperature today?"
-# 	temp_value = gets.chomp.to_i
-# 	puts "is that in fahrenheit, Celsius or Kelvin? You can exit by typing quit"
-# 	temp_unit = gets.chomp
+while not_quitting_program
+	puts "what's the temperature today?"
+	temp_value = gets.chomp.to_i
+	puts "is that in fahrenheit, Celsius or Kelvin? You can exit by typing quit"
+	temp_unit = gets.chomp
 
-# 	if temp_unit == "fahrenheit"
-# 		puts "so that's #{temp_value} in #{(temp_value - 32) / 1.8} Celsius and #{(temp_value + 459.67) / 1.8} in Kelvin"
-# 	elsif temp_unit == "Celsius"
-# 		puts "that's #{temp_value * 1.8 + 32} in fahrenheit and #{temp_value + 273.15} in Kelvin"
-# 	elsif temp_unit == "Kelvin"
-# 		puts "that's #{temp_value - 273.15} in Celsius and #{temp_value * 1.8 - 459.67} in fahrenheit"
-# 	else temp_value == "quit" || temp_unit == "quit"
-# 		puts "see you later"
-# 		not_quitting_program = false 
-# 	end 
-# end 
+	if temp_unit == "fahrenheit"
+		puts "so that's #{temp_value} in #{(temp_value - 32) / 1.8} Celsius and #{(temp_value + 459.67) / 1.8} in Kelvin"
+	elsif temp_unit == "Celsius"
+		puts "that's #{temp_value * 1.8 + 32} in fahrenheit and #{temp_value + 273.15} in Kelvin"
+	elsif temp_unit == "Kelvin"
+		puts "that's #{temp_value - 273.15} in Celsius and #{temp_value * 1.8 - 459.67} in fahrenheit"
+	else temp_value == "quit" || temp_unit == "quit"
+		puts "see you later"
+		not_quitting_program = false 
+	end 
+end 
 
 
