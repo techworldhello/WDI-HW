bdb3c226e278eef06f1dc5ca6c705afe66e4e884
Jessie <jessie.wang@hotmail.co.uk>
2018-10-15 23:02:21 +1100

    movies 2 almost complete with a bug I cant find

--- Jessie/wk05/1-mon/omdb/main.rb
+++ Jessie/wk05/1-mon/omdb/main.rb
@@ -15,21 +15,42 @@ get '/movies' do
 	title = params[:info]
 	if title == "" || title == nil 
 		redirect to('/default')
+	elsif
+		@result = HTTParty.get("http://www.omdbapi.com/?s=#{title}&apikey=2f6435d9")
 	else 
-		result = HTTParty.get("http://www.omdbapi.com/?t=#{title}&apikey=2f6435d9")
+		@result["Search"].length == 1
+		redirect to ('/result')
 	end
-	@title = result["Title"]
-	@rater = result["Ratings"][1]["Source"]
-	@rating = result["Ratings"][1]["Value"]
-	@director = result["Director"]
-	@year = result["Year"]
-	@plot = result["Plot"]
-	@poster = result["Poster"]
+	file = File.open('search_history.txt', 'a')
+	@result["Search"].each do |film|
+		file.puts film["Title"]
+	end 
+	file.close
 	erb :movies
+end 
+
+
+get '/:movie' do 
+	movie = params[:movie] 
+	# binding.pry
+	@filtered = HTTParty.get("http://www.omdbapi.com/?t=#{movie}&apikey=2f6435d9")
+	@title = @filtered["Title"]
+	@rater = @filtered["Ratings"][1]["Source"]
+	@rating = @filtered["Ratings"][1]["Value"]
+	@director = @filtered["Director"]
+	@year = @filtered["Year"]
+	@plot = @filtered["Plot"]
+	@poster = @filtered["Poster"]
+	erb :result
 end 
 
 get '/default' do
 	erb :default
+end 
+
+get '/history' do
+	@lines = File.readlines("search_history.txt")
+	erb :history
 end 
 
 # get - read
