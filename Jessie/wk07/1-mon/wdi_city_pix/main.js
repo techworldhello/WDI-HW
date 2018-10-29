0b04c27a2c39cca400067135bb3fd3c361f4fdcd
Jessie <jessie.wang@hotmail.co.uk>
2018-10-29 21:15:23 +1100

    completed city pix hw

---
+++ Jessie/wk07/1-mon/wdi_city_pix/main.js
@@ -0,0 +1,26 @@
+const cityOptions = document.getElementById('city-type');
+const cities = ['nyc', 'sf', 'la', 'sydney', 'austin'];
+
+const getCity = () => {
+	for(let i = 0; i < cities.length; i++) { 
+		let option = document.createElement('OPTION');
+		text = document.createTextNode(cities[i].toUpperCase());
+		option.appendChild(text);
+		cityOptions.insertBefore(option, cities.lastChild);
+	}
+}
+
+getCity();
+
+const docBody = document.querySelector('body');
+
+cityOptions.addEventListener('change', (e) => {
+	docBody.classList = '';
+	for(let i = 0; i < cities.length; i++) {
+		if (e.target.value == cities[i].toUpperCase()) {
+			docBody.classList.add(cities[i]);
+		} 
+	}
+})
+
+
