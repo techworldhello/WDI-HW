653794a72216aa250cbf216b3dde7bbdb15a8725
Jessie <jessie.wang@hotmail.co.uk>
2018-10-30 15:56:09 +1100

    commit 3

--- Jessie/wk07/2-tue/f7f5c8873cd7a5f9a1df53dafbcb38df/app.js
+++ Jessie/wk07/2-tue/f7f5c8873cd7a5f9a1df53dafbcb38df/app.js
@@ -2,10 +2,17 @@ const form = document.querySelector('form');
 const docBody = document.querySelector('body');
 const button = document.querySelector('button');
 
-button.addEventListener('click', (event) => {
+form.addEventListener('click', (event) => {
 	event.preventDefault();
 	docBody.style.background = '';
-
 	const input = document.querySelector('input').value;
 	docBody.style.background = input;
+
 })
+
+for(let i = 0; i <= 1000; i++) {
+	const squares = document.createElement('div');
+	squares.classList.add("squares");
+	squares.style.cssText = "width: 20px; height: 20px;";
+	docBody.appendChild(squares);
+}
