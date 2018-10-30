bae2a14cf4d7e66bcc9930835ffdb1fb49fd84b9
Jessie <jessie.wang@hotmail.co.uk>
2018-10-30 15:41:25 +1100

    commit 1(earlier commit 1 was meant to be commit 2

--- Jessie/wk07/2-tue/f7f5c8873cd7a5f9a1df53dafbcb38df/app.js
+++ Jessie/wk07/2-tue/f7f5c8873cd7a5f9a1df53dafbcb38df/app.js
@@ -1,7 +1,8 @@
 const form = document.querySelector('form');
 const docBody = document.querySelector('body');
+const button = document.querySelector('button');
 
-form.addEventListener('click', (event) => {
+button.addEventListener('click', (event) => {
 	event.preventDefault();
 	docBody.style.background = '';
 
