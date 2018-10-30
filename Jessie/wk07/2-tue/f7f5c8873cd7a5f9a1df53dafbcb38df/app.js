4d4b8947af72ae487c6a20d67d8def54305e5644
Jessie <jessie.wang@hotmail.co.uk>
2018-10-30 16:20:06 +1100

    commit 5

--- Jessie/wk07/2-tue/f7f5c8873cd7a5f9a1df53dafbcb38df/app.js
+++ Jessie/wk07/2-tue/f7f5c8873cd7a5f9a1df53dafbcb38df/app.js
@@ -1,22 +1,23 @@
 const form = document.querySelector('form');
 const docBody = document.querySelector('body');
-const button = document.querySelector('button');
 
 form.addEventListener('click', (event) => {
+	const input = document.querySelector('input').value;
 	event.preventDefault();
 	docBody.style.background = '';
-	const input = document.querySelector('input').value;
 	docBody.style.background = input;
-
+	squares(input);
 })
 
-
-for(let i = 0; i <= 1000; i++) {
-	const squares = document.createElement('div');
-	squares.classList.add("squares");
-	squares.style.cssText = "width: 20px; height: 20px;";
-	squares.addEventListener('click', (e) => {
-		e.target.style.background = 'green';
-	})
-	docBody.appendChild(squares);
+function squares(inputCol) {
+	for(let i = 0; i <= 1000; i++) {
+		const squares = document.createElement('div');
+		squares.classList.add("squares");
+		squares.style.cssText = "width: 20px; height: 20px; background-color: white;";
+		squares.addEventListener('click', (e) => {
+			e.target.style.background = inputCol;
+		})
+		docBody.appendChild(squares);
+	}
 }
+
