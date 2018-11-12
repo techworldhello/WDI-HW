a4d50af24a1f368195afc67d782767981fe063e0
Jessie <jessie.wang@hotmail.co.uk>
2018-11-12 16:10:11 +1100

    hogwarts complete

--- Jessie/wk08/5-fri/hogwarts/app/controllers/students_controller.rb
+++ Jessie/wk08/5-fri/hogwarts/app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
 
   def index
-    @student = Student.all
+    @students = Student.all
   end
 
   def show
-    @student = Student.find(:id)
+    @student = Student.find(params[:id])
   end
 
   def new
